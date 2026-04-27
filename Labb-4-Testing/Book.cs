cd77f7469a0b44e15c0737854697eb11ded0aaeb
VISORKILLEN <viktor.gustavsson@live.se>
2026-04-27 10:15:33 +0200

    Add project files.

---
+++ Labb-4-Testing/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace Labb_4_Testing
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public int PublicationYear { get; set; }
+        public bool IsBorrowed { get; set; }
+        public DateTime? BorrowDate { get; set; }
+
+        public Book(string title, string author, string isbn, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            PublicationYear = publicationYear;
+            IsBorrowed = false;
+            BorrowDate = null;
+        }
+    }
+}
---
+++ Labb-4-Testing/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Labb_4_Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibrarySystem library = new LibrarySystem();
+            UserInterface.DisplayMenu(library);
+        }
+    }
+}
